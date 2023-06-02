c1ba038b9ef6bada9487b76be315283fe2ebcf4d
DailyGrow <858519155@qq.com>
2023-06-01 17:42:55 -0700

    add product service API

--- springpro-product/src/test/java/com/atspring/springpro/product/SpringproProductApplicationTests.java
+++ springpro-product/src/test/java/com/atspring/springpro/product/SpringproProductApplicationTests.java
@@ -27,56 +27,7 @@ public class SpringproProductApplicationTests {
 
     @Autowired
     BrandService brandService;
-    /*
-    @Autowired
-    OSS ossClient;
 
-    @Test
-    public void testUpload() {
-        String endpoint = "oss-us-west-1.aliyuncs.com";
-        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-        String accessKeyId = "LTAI5t8gSRopfSGGtQZK27Yw";
-        String accessKeySecret = "Uz10a1ozHROQvF0W65ZGxyRLJ86n6c";
-        // 填写Bucket名称，例如examplebucket。
-        String bucketName = "springpro1";
-        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
-        String objectName = "exampledir/xiaomi.png";
-        // 填写本地文件的完整路径，例如D:\\localpath\\examplefile.txt。
-        // 如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
-        String filePath = "E:\\summer2023\\xiaomi.png";
-
-        // 创建OSSClient实例。
-        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
-
-        try {
-            InputStream inputStream = new FileInputStream(filePath);
-            // 创建PutObjectRequest对象。
-            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectName, inputStream);
-            // 创建PutObject请求。
-            PutObjectResult result = ossClient.putObject(putObjectRequest);
-        } catch (OSSException oe) {
-            System.out.println("Caught an OSSException, which means your request made it to OSS, "
-                    + "but was rejected with an error response for some reason.");
-            System.out.println("Error Message:" + oe.getErrorMessage());
-            System.out.println("Error Code:" + oe.getErrorCode());
-            System.out.println("Request ID:" + oe.getRequestId());
-            System.out.println("Host ID:" + oe.getHostId());
-        } catch (ClientException ce) {
-            System.out.println("Caught an ClientException, which means the client encountered "
-                    + "a serious internal problem while trying to communicate with OSS, "
-                    + "such as not being able to access the network.");
-            System.out.println("Error Message:" + ce.getMessage());
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } finally {
-            if (ossClient != null) {
-                ossClient.shutdown();
-            }
-        }
-        System.out.println("upload complete");
-
-    }
-    */
     @Test
      public void contextLoads() {
         /*
